f84ac68486e3e42cdceb1598f6fc9f4f1545f26f
Apophis072 <jeffreychen846@gmail.com>
2024-07-08 13:37:40 -0400

    List Maker

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        Scanner in = new Scanner(System.in);
+        String input = "";
+        ArrayList<String> list = new ArrayList<>();
+
+        input = SafeInput.getNonZeroLenString(in, "Choose one of the follow: \nA: Add to list\tD: Delete from list\tI: Insert to list\tP: to print list\tQ: Quit program");
+
+        if (input.equalsIgnoreCase("A"))
+        {
+
+        }
+
+
     }
+
+    //add to the array "list"
+    private static void add ()
+    {
+        String addItem = "";
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+
+        addItem = SafeInput.getNonZeroLenString(in, "Input to item to add");
+        list.add(addItem);
+
+    }
+
 }
